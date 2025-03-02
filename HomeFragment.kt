7ec905cbd7dade1cd5ef8e9ddbe84e1d67cdf0ee
Elad Shirazi <118260028+eladshirazi@users.noreply.github.com>
2025-03-02 12:59:17 +0100

    Add files via upload

---
+++ HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.plantify
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+
+class HomeFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        auth = FirebaseAuth.getInstance()
+
+        return view
+    }
+}
